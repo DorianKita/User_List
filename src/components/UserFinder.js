d4b195bd3812632b15983a27c6ad5f7432cc8fc3
Dorian Kita <dorian.kita90@gmail.com>
2024-03-20 21:47:06 +0100

    added context

--- src/components/UserFinder.js
+++ src/components/UserFinder.js
@@ -1,15 +1,11 @@
-import { Fragment, useState, useEffect, Component} from 'react';
-
+import { Fragment, Component} from 'react';
+import UsersContext from '../store/users-context.js';
 import Users from './Users';
 import classes from './UserFinder.module.css';
 
-const DUMMY_USERS = [
-  { id: 'u1', name: 'Max' },
-  { id: 'u2', name: 'Manuel' },
-  { id: 'u3', name: 'Julie' },
-];
 
 class UserFinder extends Component{
+    static contextType = UsersContext;
     
     constructor(){
         super()
@@ -21,12 +17,12 @@ class UserFinder extends Component{
 
     componentDidMount(){
         // fetching some dummmy data from server 
-        this.setState({filteredUsers: DUMMY_USERS});
+        this.setState({filteredUsers: this.context.users});
     }
 
     componentDidUpdate(prevProps, prevState){
         if(prevState.searchTerm !== this.state.searchTerm) {
-            this.setState({filteredUsers:DUMMY_USERS.filter((user) => user.name.includes(this.state.searchTerm))})
+            this.setState({filteredUsers:this.context.users.filter((user) => user.name.includes(this.state.searchTerm))})
 
         }
     }
